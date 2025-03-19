4f3b64b4a53ac740073e96a1651d0f7b7a08dff2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-19 08:13:38 -0300

    Tarefa Musica concluida

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"
++---------------------------------+
+| Hit The Road Jack - Ray Charles |
++---------------------------------+
+");
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Blue;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+Console.WriteLine(@"Hit the road, Jack, and don't you come back
+No more, no more, no more, no more
+Hit the road, Jack, and don't you come back no more
+What you say");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine(@"Woah, woman, oh, woman, don't treat me so mean
+You're the meanest old woman that I've ever seen
+I guess if you said so
+I'd have to pack my things and go that's right");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine();
+Console.WriteLine(@"Hit the road, Jack, and don't you come back
+No more, no more, no more, no more
+Hit the road, Jack, and don't you come back no more
+What you say?");
+Console.WriteLine();
+Console.WriteLine(@"Hit the road, Jack, and don't you come back
+No more, no more, no more, no more
+Hit the road, Jack, and don't you come back no more");
+
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine(@"Now, baby, listen, baby, don't ya treat me this way
+'Cause I'll be back on my feet some day
+Don't care if you do, 'cause it's understood
+You ain't got no money, you just ain't no good
+Well, I guess if you say so
+I'd have to pack my things and go that's right");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"
+Hit the road, Jack, and don't you come back
+No more, no more, no more, no more
+Hit the road, Jack, and don't you come back no more
+What you say?");
+
+Console.WriteLine(@"
+Hit the road, Jack, and don't you come back
+No more, no more, no more, no more
+Hit the road, Jack, and don't you come back no more");
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+Well
+Don't you come back no more");
+
+Console.WriteLine(@"
+Uh, what you say?
+Don't you come back no more");
+
+Console.WriteLine(@"
+I didn't understand you!
+Don't you come back no more");
+
+Console.WriteLine(@"
+You can't mean that!
+Don't you come back no more");
+
+Console.WriteLine(@"
+Oh, no, baby, please!
+Don't you come back no more");
+
+Console.WriteLine(@"
+What you tryin' to do to me?
+Don't you come back no more");
+
+Console.WriteLine(@"
+Oh, don't treat me like that!
+Don't you come back no more
+");
+Console.WriteLine(@"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+");
